6fd0d3fedd6d9cf0febf0ebc85d61f0b0fd59040
Usama Meer <usamameer256@gmail.com>
2024-07-04 12:05:54 +0500

    created ListingVM model and added IFormFile

---
+++ Models/ListingVM.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ValueBid.Models
+{
+    public class ListingVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsSold { get; set; }=false;
+        
+        //added for Image file
+        public IFormFile Image { get; set; }
+
+        [Required]
+        public string IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+
+        public IdentityUser User { get; set; }
+
+        public List<Bid> Bids { get; set; }
+
+        public List<Comment> Comments { get; set; } 
+
+
+    }
+}
