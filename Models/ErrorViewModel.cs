d1a1ba8549d5c8f7e1f5a3a4bcacc28bf2491931
Usama Meer <usamameer256@gmail.com>
2024-07-04 10:33:48 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ValueBid.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
