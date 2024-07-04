f5842b6cb9fa33ec70023d5c331ac676fef36c5d
Usama Meer <usamameer256@gmail.com>
2024-07-04 11:23:42 +0500

    added scoped service to the program.cs file with IListingsService as a interface and the implementation class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ValueBid.Data;
+using ValueBid.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,10 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+//adding scoped service(IListingsService as parameter of ListingsService)
+builder.Services.AddScoped<IListingsService, ListingsService>();
+
 
 var app = builder.Build();
 
