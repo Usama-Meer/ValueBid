02d8b391cc5bc98ac00cf0ad9c4392cc70aff3af
Usama Meer <usamameer256@gmail.com>
2024-07-04 11:07:44 +0500

    created ListingsService and implemented IListingsService interface in it

---
+++ Data/Services/ListingsService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ValueBid.Models;
+
+namespace ValueBid.Data.Services
+{
+    public class ListingsService : IListingsService
+    {
+        public readonly ApplicationDbContext _context;
+        public IQueryable<Listing> GetAll()
+        {
+            var applicationDbContext=_context.Listings.Include(I=>I.User);
+            return applicationDbContext;
+        }
+
+        public ListingsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
